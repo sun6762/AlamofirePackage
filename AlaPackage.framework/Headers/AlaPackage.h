d0bebbff41b551a6aa19579a576f25610373504a
bosun <Eports@bosun2021>
2024-04-08 10:14:44 +0800

    first commit

---
+++ AlaPackage.framework/Headers/AlaPackage.h
@@ -0,0 +1,18 @@
+//
+//  AlaPackage.h
+//  AlaPackage
+//
+//  Created by bobo on 2024/4/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlaPackage.
+FOUNDATION_EXPORT double AlaPackageVersionNumber;
+
+//! Project version string for AlaPackage.
+FOUNDATION_EXPORT const unsigned char AlaPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlaPackage/PublicHeader.h>
+
+
